ec1434ef681050a7abff3d21dfe61113ecbbb784
MrCranito <moreau.totor@gmail.com>
2024-02-05 16:35:40 +0100

    setup columns && call service oninit

--- cats_frontend/src/app/modules/cats/pages/cats-list/cats-list.component.ts
+++ cats_frontend/src/app/modules/cats/pages/cats-list/cats-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ICat } from "../../models/cat.model";
 import { CatsService } from "../../services/cats.service";
 
@@ -6,9 +6,19 @@ import { CatsService } from "../../services/cats.service";
     selector: 'app-cats-list',
     templateUrl: './cats-list.component.html'
 })
-export class CatsListComponent {
+export class CatsListComponent implements OnInit {
 
     cats: ICat[] = [];
+    cols: { field: string, header: string }[] = [
+        { field: 'name', header: 'Name' },
+        { field: 'breed', header: 'Breed' },
+        { field: 'birthday', header: 'Birthday' },
+        { field: 'description', header: 'Description' }
+      ]
 
     constructor(private catsService: CatsService) { }
+
+    ngOnInit(): void {
+        this.catsService.get().subscribe(cats => this.cats = cats);
+    }
 }
