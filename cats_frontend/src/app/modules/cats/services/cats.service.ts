84fe0c3b774265df2f77e8d61db750f84f7d3f0c
MrCranito <moreau.totor@gmail.com>
2024-02-05 16:22:38 +0100

    inject http service et setup environment api_url

--- cats_frontend/src/app/modules/cats/services/cats.service.ts
+++ cats_frontend/src/app/modules/cats/services/cats.service.ts
@@ -1,9 +1,28 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { ICat } from "../models/cat.model";
+import { Observable } from "rxjs";
+import { environment } from "src/app/environment/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CatsService {
     constructor(private http: HttpClient) { }
+
+    get(): Observable<ICat[]> {
+        return this.http.get<ICat[]>(`${environment.api_url}/v1/cats`);
+    }
+
+    create(cat: ICat): Observable<ICat> {
+        return this.http.post<ICat>(`${environment.api_url}/v1/cats`, cat);
+    }
+
+    update(cat: ICat): Observable<ICat> {
+        return this.http.patch<ICat>(`${environment.api_url}/v1/cats/${cat.id}`, cat);
+    }
+
+    delete(id: number): Observable<any> {
+        return this.http.delete(`${environment.api_url}/v1/cats/${id}`);
+    }
 }
--- cats_frontend/src/app/modules/cats/services/comments.service.ts
+++ cats_frontend/src/app/modules/cats/services/comments.service.ts
@@ -1,9 +1,28 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { IComment } from "../models/comment.model";
+import { Observable } from "rxjs";
+import { environment } from "src/app/environment/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CommentsService {
     constructor(private http: HttpClient) { }
+
+    get(): Observable<IComment[]> {
+        return this.http.get<IComment[]>(`${environment.api_url}/v1/comments`);
+    }
+
+    create(comment: IComment): Observable<IComment> {
+        return this.http.post<IComment>(`${environment.api_url}/v1/comments`, comment);
+    }
+
+    update(comment: IComment): Observable<IComment> {
+        return this.http.patch<IComment>(`${environment.api_url}/v1/comments/${comment.id}`, comment);
+    }
+
+    delete(id: number): Observable<any> {
+        return this.http.delete(`${environment.api_url}/v1/comments/${id}`);
+    }
 }
